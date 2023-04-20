3295c976aa43bb00d937f194736ae0730c075393
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-20 14:46:24 +0530

    initial commit

---
+++ book-store-management/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.bookStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
