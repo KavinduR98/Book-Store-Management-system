3592e2a0f34a34cb0fbd6c76d3222a8ed65f0f4c
@kavinduushan07 <kavinduushan07@gmail.com>
2023-04-22 15:50:07 +0530

    update Home image, create book registration page & Get all book info

--- book-store-management/src/main/java/com/bookStore/controller/BookController.java
+++ book-store-management/src/main/java/com/bookStore/controller/BookController.java
@@ -1,10 +1,20 @@
 package com.bookStore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookStore.entity.Book;
+import com.bookStore.service.BookService;
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookService service;
+	
 	@GetMapping("/")
 	public String home() {
 		return "home";
@@ -13,6 +23,19 @@ public class BookController {
 	public String bookRegister() {
 		return "bookRegister";
 	}
+	@GetMapping("/available_books")
+	public ModelAndView getAllBook() {
+		List<Book>list=service.getAllBook();
+//		ModelAndView m=new ModelAndView();
+//		m.setViewName("bookList");
+//		m.addObject("book", list);
+		return new ModelAndView("bookList", "book", list);
+	}
 	
-
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	
 }
